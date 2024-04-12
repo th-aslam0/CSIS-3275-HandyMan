cc9c5065af1db8348d42f412cc45b316c4f93d7a
Tanmay Dewan <tddewan2@gmail.com>
2024-04-12 10:33:04 -0700

    Total Earning

--- src/components/NHandyHomePage.vue
+++ src/components/NHandyHomePage.vue
@@ -1,40 +1,40 @@
 <template>
-   <div>
-        <NavBar/>
-    </div>
-    <Card>
-        <template #title>Total Earnings</template>
-        <template #content>
-            <p class="m-0">
-                
-            </p>
-        </template>
-    </Card>
-  
-  </template>
-  
-  <script>
-  
+  <div>
+    <NavBar/>
+  </div>
+  <Card>
+    <template #title>Total Earnings</template>
+    <template #content>
+      <p class="m-0">{{"$"+generateRandomNumber() }}</p>
+    </template>
+  </Card>
+</template>
+
+<script>
 import Card from 'primevue/card';
 import NavBar from "./NavBar.vue"
 
-  
-  export default {
-    name: 'NHandyHomePage',
-    components: {
+export default {
+  name: 'NHandyHomePage',
+  components: {
+    NavBar,
+    Card
+  },
+  data() {
+    return {
+      searchQuery: '',
       NavBar,
-      Card
-    },
-    data() {
-      return {
-        searchQuery: '',
-        NavBar,
-      };
+    };
+  },
+  methods: {
+    generateRandomNumber() {
+      // Generate a random number between 999 and 1500
+      return Math.floor(Math.random() * (1500 - 999 + 1)) + 999;
     }
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  }
+};
+</script>
+
+<style scoped>
+/* Add your scoped styles here */
+</style>
